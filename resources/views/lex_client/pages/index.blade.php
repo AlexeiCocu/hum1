f0a17ff1f8785376e4d65ff40f2789a430443aa2
AlexeiCocu <alexei.cocu@gmail.com>
2021-07-01 11:22:12 +0300

    d

--- resources/views/lex_client/pages/index.blade.php
+++ resources/views/lex_client/pages/index.blade.php
@@ -21,7 +21,7 @@
     <link rel="apple-touch-icon" href="{{asset('images/icons/icon-192x192.png')}}">
     <meta name="apple-mobile-web-app-status-bar" content="red">
 
-{{--    <link rel="manifest" href="{{asset('manifest.json')}}">--}}
+    <link rel="manifest" href="{{asset('manifest.json')}}">
 
     <!-- Calendly link widget begin -->
     <link href="https://calendly.com/assets/external/widget.css" rel="stylesheet">
@@ -125,6 +125,6 @@
 <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=60c3a5d304798b7da774cbd0" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 {{--<script src="{{asset('lex_client/js/webflow.js')}}" type="text/javascript"></script>--}}
 <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
-{{--<script src="{{asset('js/serviceWorker.js')}}"></script>--}}
+<script src="{{asset('js/serviceWorker.js')}}"></script>
 </body>
 </html>
